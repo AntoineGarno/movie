477bb80a991d77ab0982fe8e6b4aaf16f247bb54
Vomito <maxrouthier734@gmail.com>
2022-07-11 09:05:42 -0400

    Fiche Acteur Work bobé

--- src/components/FicheActor.js
+++ src/components/FicheActor.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 import React from "react";
-
+import image from "./image.jpg"
 import { useEffect, useState } from "react";
-
 const FicheActor = (props) => {
   const [pop, setPop] = useState([]);
 
@@ -26,7 +25,7 @@ const FicheActor = (props) => {
           <img
             src={"https://image.tmdb.org/t/p/w200" + pop.profile_path}
             alt="poster"
-            className="img-fluid rounded p-3 imageActor"
+            className="img-fluid rounded-3 p-3 imageActor"
           />
         </div>
 
@@ -40,22 +39,104 @@ const FicheActor = (props) => {
           <p className="text-danger">METTRE LIEU DE NAISSANCE CI DANS API</p>
         </div>
 
-        <div className="container">
-          <div className="row ">
-            <ul className="col-5 d-flex justify-content-evenly flex-row p-1  liActor">
-              <li>Résumé</li>
-              <li>Bande annonces</li>
-              <li>Vidéos</li>
-              <li>Photos</li>
-            </ul>
-            <ul className="col-7 d-flex justify-content-center flex-row p-1 liActor">
-              <li className="col-3">Acteur</li>
-              <li className="col-3">Producteur</li>
-              <li className="col-3">Directeur</li>
+        <div className="row">
+          <ul className="col-5 d-flex justify-content-evenly flex-row p-1 liActor">
+            <li className="pt-1">Résumé</li>
+            <li className="pt-1">Bande annonces</li>
+            <li className="pt-1">Vidéos</li>
+            <li className="pt-1">Photos</li>
+          </ul>
+
+          <div className="col-7 px-4">
+            <ul className="col-8 d-flex justify-content-end flex-row p-1 ml-4">
+              <li className="px-4 py-2 mx-3 actoreBadge">Acteur</li>
+              <li className="px-4 py-2 mx-3 actoreBadge">Producteur</li>
+              <li className="px-4 py-2 mx-3 actoreBadge">Directeur</li>
             </ul>
           </div>
-            <hr/>
+
+          <hr className="hr" />
+          <div className="row">
+            <div className="col-5 mx-2  d-inline-flex flex-row justify-content-evenly filmographie">
+              <h4 className="py-2 mx-4">Connu pour</h4>
+              <span class="mt-4 degrade-horizontal"></span>
+            </div>
+          </div>
+          <div className="row col-lg-12 col-sm-6 m-4 ">
+            <div className="col-2"></div>
+            <div className=" col-8 d-flex flex-row">
+              <div className="p-2 col bg-info">Film 1</div>
+              <div className="p-2 col bg-danger">Film 2</div>
+              <div className="p-2 col bg-warning">Film 3</div>
+              <div className="p-2 col bg-success">Film 4</div>
+              <div className="col-2"></div>
+            </div>
+          </div>
+
+          <hr className="hr2" />
+
+          <div className="row">
+            <div className="col-5 d-inline-flex flex-row justify-content-evenly filmographie ">
+              <h4 className="py-2 mx-4">Filmographie</h4>
+              <span class="mt-4 degrade-horizontal"></span>
+            </div>
+            <div className="col-1 d-flex flex-row">
+              <div className="col-6 py-2">ARROW LEFT</div>
+              <div className="col-6 py-2">ARROW RIGHT</div>
+            </div>
+          </div>
+
+          <div className="col-9 d-flex flex-row tableFilmographie">
+            <table id="tableActor" className="p-2 px-3 col-8 text-start">
+              <tr className="bg-dark">
+                <th className="col-10">Acteur</th>
+                <th className="col-2">Date</th>
+              </tr>
+              <tr>
+                <td className="p-3">Jeanne du Barry (pre-production)<br />Louis XV</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td className="p-3">Puffins (TV Mini Series short) (filming)<br />
+                  Johnny Puff<br />
+                  - Let's Dance ... Johnny Puff<br />
+                  - Fishing Competition ... Johnny Puff<br />
+                  - Whistle it off ... Johnny Puff<br />
+                  - A very special band ... Johnny Puff<br />
+                  - Tac's glasses ... Johnny Puff<br />
+                  Show all 70 episodes</td>
+                <td className="text-start">2021-2022</td>
+              </tr>
+              <tr>
+                <td>Centro comercial Moctezuma</td>
+                <td>2022</td>
+              </tr>
+              <tr>
+                <td>Ernst Handel</td>
+                <td>2017</td>
+              </tr>
+              <tr>
+                <td>Island Trading</td>
+                <td>2011</td>
+              </tr>
+
+            </table>
+          </div>
+
         </div>
+        <div className="row">
+          <div className="col-5 d-inline-flex flex-row justify-content-evenly filmographie ">
+            <h4 className="py-2 mx-3 text-start col-2">Photo</h4>
+            <span class="mt-4 degrade-horizontal"></span>
+          </div>
+          <div className="col-1 d-flex flex-row">
+            <div className="col-6 py-2">ARROW LEFT</div>
+            <div className="col-6 py-2">ARROW RIGHT</div>
+          </div>
+        </div>
+        <footer>
+          <div></div>
+        </footer>
       </div>
     </div>
   );

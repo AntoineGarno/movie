1cf3559adfc926aed27e228e9b7a550bb4cfdf0d
minigarni <Minigarni>
2022-07-19 09:29:46 -0400

    change css

--- src/components/CategoriesList.js
+++ src/components/CategoriesList.js
@@ -2,12 +2,31 @@ import axios from "axios";
 import React from "react";
 import Movie from "./Movie";
 import { useEffect, useState } from "react";
+import Typography from "@mui/material/Typography";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const CategoriesList = () => {
   const [pop, setPop] = useState([]);
   const [id, setId] = useState("");
   const [genre, setGenre] = useState([]);
   const [page, setPage] = useState(1);
+
+  const theme = createTheme({
+    palette: {
+      text: {
+        primary: "#fffff",
+      },
+      secondary: {
+        main: "#ffffff",
+      },
+    },
+  });
+
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
 
   const movieURL =
     "https://api.themoviedb.org/3/discover/movie?api_key=a67b57849deb687f2cd49d7a8298b366&language=en-US&with_genres=";
@@ -33,9 +52,19 @@ const CategoriesList = () => {
           </button>
         ))}
       </div>
-      <div>
-        <button onClick={() => setPage(page - 1)}>Back</button>
-        <button onClick={() => setPage(page + 1)}>Next</button>
+      <div className="d-flex flex-row justify-content-center my-3 ">
+        <ThemeProvider theme={theme}>
+          <Stack spacing={2}>
+            <Typography>Page: {page}</Typography>
+            <Pagination
+              count={50}
+              page={page}
+              onChange={handleChange}
+              color="secondary"
+              variant="text"
+            />
+          </Stack>
+        </ThemeProvider>
       </div>
 
       {

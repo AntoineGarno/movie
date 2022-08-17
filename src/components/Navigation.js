6a29eedc412108828d4c22dd9e9e19d675ea7b05
Vomito <maxrouthier734@gmail.com>
2022-08-17 17:48:32 -0400

    Job 17 Aout Nav Mobile

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,17 +2,22 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../logo.png";
 
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
 const Navigation = () => {
   return (
     <div>
-      <div className="d-flex align-items-center container-fluid p-3 ">
-        <div className="col-sm-1 col-md-1 col-lg-4 ">
+      <div className="d-flex align-items-center container-fluid">
+        <div className="col-sm-1 col-md-1 col-lg-4 d-sm-none d-md-none d-lg-block ">
           <NavLink exact to="/" activeClassName="nav-active">
             <img src={logo} alt="Logo" className="logo" />
           </NavLink>
         </div>
 
-        <div className="navigation rounded-pill col-sm-6 col-md-8 col-lg-5 m-3 d-flex flex-row justify-content-between p-3">
+        <div className="navigation rounded-pill col-sm-12 col-md-12 col-lg-5 m-3 d-flex flex-row justify-content-between p-3  d-sm-none d-md-none d-lg-block">
           <NavLink exact to="/" activeClassName="nav-active">
             Home
           </NavLink>
@@ -39,6 +44,34 @@ const Navigation = () => {
           />
         </div>
       </div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="d-lg-none d-md-block navigationMobile" >
+        <Container>
+              <Navbar.Brand href="#home">
+                <NavLink exact to="/" activeClassName="nav-active">
+                  <img src={logo} alt="Logo" className="logo" id="logoMobile" />
+                </NavLink>
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                  <NavLink exact to="/" activeClassName="nav-active" className="fs-2">
+                    Home
+                  </NavLink>
+                  <NavLink exact to="/search" activeClassName="nav-active" className="fs-2">
+                    Search
+                  </NavLink>
+                </Nav>
+                <Nav>
+                  <NavLink exact to="/categories" activeClassName="nav-active" className="fs-2">
+                    Categories
+                  </NavLink>
+                  <NavLink exact to="/popular" activeClassName="nav-active" className="fs-2">
+                    Popular
+                  </NavLink>
+                </Nav>
+              </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 };
